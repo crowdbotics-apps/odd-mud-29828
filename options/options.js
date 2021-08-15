9bdf2ef70dadb7c2d81f396b207aedc8f4b297fb
Crowdbotics <team@crowdbotics.com>
2021-08-15 17:35:01 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "odd_mud_29828Identifier",
+  url: "https://odd_mud_29828Identifier.botics.co",
+  api: "https://odd_mud_29828Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
